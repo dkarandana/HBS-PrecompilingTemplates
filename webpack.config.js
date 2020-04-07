199242757c15540d66dd2685a70fc2976a4e5138
Dhananjaya <dhananjayakarandana@gmail.com>
2020-04-07 15:01:01 +0530

    initial webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+
+const webpack = require('webpack');
+const OUT_DIR_ABS = path.resolve('./dist');
+const IS_PROD = (process.env.NODE_ENV === 'prod') ? true : false;
+
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+module.exports = {
+  name: 'js-files',
+  watch: !IS_PROD,
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: OUT_DIR_ABS,
+    filename: './js/[name].bundle.js'
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      // browse to http://localhost:3000/ during development,
+      // ./public directory is being served
+      host: 'localhost',
+      port: 3000,
+      server: { 
+        baseDir: ['public'] 
+      },
+      serveStatic: [{
+        route: '/dist',
+        dir: 'dist'
+      }]
+    })
+  ]
+};
